153b9db4ad6af81e7998ae1ce8ad006b4df572a6
Ivana Runje <irunjeee@gmail.com>
2023-06-24 13:02:53 +0200

    Created login page with some basic functionality, future todos are form validation, style improving and setting up page redirect after successful form submission

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="HomeDescription">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+          corporis veniam unde doloribus nihil at commodi. Voluptatum unde
+          dolore, quasi quaerat, laboriosam assumenda vero sit perferendis,
+          repudiandae doloribus esse incidunt?
+        </p>
+      </div>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    console.log("Username: ", username);
+    console.log("Room: ", room);
+
+    setUsername("");
+    setRoom("");
+  };
+  return (
+    /* Form with a username input field, room selection dropdown, and a login button.
+     *** TODO: form validation and style*/
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Chat Room:</label>
+          <select
+            id="room"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          >
+            <option value="">Select a room to join</option>
+            <option value="DataGeeks">DataGeeks</option>
+            <option value="WebDev">WebDev</option>
+            <option value="BackendHeroes">BackendHeroes</option>
+            <option value="MobileAppDev">MobileAppDev</option>
+            <option value="UX/UI">UX/UI</option>
+          </select>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;

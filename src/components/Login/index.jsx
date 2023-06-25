f37e0def144226c0a6b0c7b4d0ef24e0db770311
Ivana Runje <irunjeee@gmail.com>
2023-06-25 12:59:13 +0200

    Implemented form validation and added page redirection upon successful form submission

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [error, setError] = useState({});
+  const navigate = useNavigate();
 
+  // Form submit function
   const submitFormHandler = (e) => {
     e.preventDefault();
     console.log("Username: ", username);
     console.log("Room: ", room);
 
-    setUsername("");
-    setRoom("");
+    const errorMsg = validateData();
+    console.log("msgErrors: ", errorMsg.username);
+    console.log("msgErrors: ", errorMsg.room);
+    console.log("msgErrors: ", errorMsg.length);
+
+    if (!errorMsg.username && !errorMsg.room) {
+      // go to chatRoom component
+      console.log("msgErrors: OK ");
+      navigate("/chatRoom");
+      // reset values
+      setUsername("");
+      setRoom("");
+      setError({});
+    } else {
+      console.log("msgErrors: NIJE OK ");
+      setError(errorMsg);
+    }
   };
+
+  // Form validation function
+  const validateData = () => {
+    const error = {};
+
+    if (username.trim() === "") {
+      error.username = "Username is required";
+    } else if (username.length < 2) {
+      error.username = "Username must be at least 2 characters long";
+    }
+
+    if (room === "") {
+      error.room = "Please select a room";
+    }
+    return error;
+  };
+
   return (
     /* Form with a username input field, room selection dropdown, and a login button.
-     *** TODO: form validation and style*/
+     *** TODO: style*/
     <div>
       <h2>Login</h2>
       <form onSubmit={submitFormHandler}>
@@ -43,6 +79,8 @@ function Login() {
             <option value="UX/UI">UX/UI</option>
           </select>
         </div>
+        {error.username ? <p className="error">{error.username}</p> : null}
+        {error.room ? <p className="error">{error.room}</p> : null}
         <button type="submit">Login</button>
       </form>
     </div>
